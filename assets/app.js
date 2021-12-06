041f7d83929cb5bce042801720dbeccef04df26a
raulcejas04 <raulcejas04@hotmail.com>
2021-12-05 23:25:24 -0300

    sedroExtra2 con webapck, no borren el package.json por favor

--- assets/app.js
+++ assets/app.js
@@ -7,11 +7,20 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/style.scss';
+import './styles/poncho.css';
+import './styles/style.css';
+
+import Bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 //import './styles/index.css';
 //import './styles/docs.min.css';
 
 import $ from 'jquery';
 import jQuery from 'jquery';
+global.$ = global.jQuery = $;
 
 import './bootstrap';
+
+import validator from 'validator';
+global.validator = validator;
